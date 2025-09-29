df949abf306abd31905662070a3b79c3fc542b39
boukroun.younes <boukroun.younes@gmail.com>
2025-09-29 11:48:26 +0200

    fetch and display popular movies

--- js/script.js
+++ js/script.js
@@ -14,6 +14,34 @@ links.forEach(link => {
 });
 }
 
+async function displayPopularMovies() {
+    const { results } = await fetchData('movie/popular');
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">    
+            ${
+                movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">` : '<img src="images/no-image.jpg" class="card-img-top" alt="No Image Available">'
+            }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">Release Date: ${movie.release_date}</p>
+            </div>
+        `;
+        document.getElementById('popular-movies').appendChild(div);
+    });
+}
+
+// Fetch data from API
+async function fetchData(endpoint) {
+    const apiKey = 'fb36391bf7f0872322162fb66d71a790';
+    const apiUrl = `https://api.themoviedb.org/3/`;
+    const response = await fetch(`${apiUrl}${endpoint}?api_key=${apiKey}&language=en-US`);
+    const data = await response.json();
+    return data;
+}
 
 
 //init app
@@ -37,6 +65,7 @@ function init() {
             break;
     }
     highlightActiveLink();
+    displayPopularMovies();
 }
 
 document.addEventListener('DOMContentLoaded', init);

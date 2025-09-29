b3d479a9503bf07b0835edd99ea55db4508ac973
boukroun.younes <boukroun.younes@gmail.com>
2025-09-29 15:25:00 +0200

    display movie details

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const global = {
 
 //highlight active link
 function highlightActiveLink () {
-const links = document.querySelectorAll('.nav-link');
-links.forEach(link => {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
     if (link.getAttribute('href') === global.currentPage) {
         link.classList.add('active');
     } else {
@@ -55,6 +55,50 @@ async function displayPopularShows() {
     });
 }
 
+async function displayMoviesDetails() {
+    const movieId = window.location.search.split('=')[1];
+    const movie = await fetchData(`movie/${movieId}`);
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+            <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+                ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> ${addCommasNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> ${addCommasNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map(company => company.name).join(', ')}</div>
+        </div> `;
+    document.querySelector('#movie-details').appendChild(div);
+}
+
 
 function showSpinner() {
     document.querySelector('.spinner').classList.add('show');
@@ -78,6 +122,10 @@ async function fetchData(endpoint) {
 }
 
 
+function  addCommasNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 //init app
 function init() {
     switch (global.currentPage) {
@@ -89,10 +137,10 @@ function init() {
             displayPopularShows();
             break;
         case '/movie-details.html':
-            console.log('Movie Details Page');
+            displayMoviesDetails();
             break;
         case '/tv-details.html':
-            console.log('TV Details Page');
+            displayMoviesDetails();
             break;
         case '/search.html':
             console.log('Search Page');
